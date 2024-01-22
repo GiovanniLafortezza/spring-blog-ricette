d618e46237ae4c9602461bdf34f0bc599180fab4
GiovanniLafortezza <recordeknot388@gmail.com>
2024-01-22 09:31:16 +0100

    set up project

---
+++ spring-blog-ricette/src/main/java/org/lessons/jaita/blogricette/SpringBlogRicetteApplication.java
@@ -0,0 +1,13 @@
+package org.lessons.jaita.blogricette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBlogRicetteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBlogRicetteApplication.class, args);
+	}
+
+}
---
+++ spring-blog-ricette/src/test/java/org/lessons/jaita/blogricette/SpringBlogRicetteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lessons.jaita.blogricette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBlogRicetteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
